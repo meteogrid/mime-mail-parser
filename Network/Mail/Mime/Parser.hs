baec0cb35ad028c0cc6a982d7583878fcfec35b5
Alberto Valverde Gonzalez <alberto@meteogrid.com>
2015-03-02 19:31:46 +0100

    exposed utils

--- Network/Mail/Mime/Parser.hs
+++ Network/Mail/Mime/Parser.hs
@@ -15,9 +15,18 @@ module Network.Mail.Mime.Parser (
     message
   , mime_message_headers
   , body
+  , parseMessage
   , module Network.Mail.Mime.Parser.Types
+  , module Network.Mail.Mime.Parser.Util
 ) where
 
+import Control.Applicative ((<*))
+import Data.Attoparsec.ByteString.Char8 (parseOnly, endOfInput)
+import Data.ByteString.Char8 (ByteString)
 import Network.Mail.Mime.Parser.Types
+import Network.Mail.Mime.Parser.Util
 import Network.Mail.Mime.Parser.Internal.Rfc2045 (mime_message_headers)
 import Network.Mail.Mime.Parser.Internal.Rfc2046 (message, body)
+
+parseMessage :: ByteString -> Either String Message
+parseMessage = parseOnly (message <* endOfInput)
